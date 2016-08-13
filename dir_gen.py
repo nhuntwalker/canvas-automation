afe3b83ef16ce79f4da1dbb68b3ffb4905040206
Will Weatherford <weatherford.william@gmail.com>
2016-08-13 01:52:02 -0700

    Working creation of all week/assignment/student directory tree.

--- dir_gen.py
+++ dir_gen.py
@@ -5,6 +5,9 @@ import re
 import requests
 from string import punctuation
 
+HERE = os.path.dirname(__file__)
+ROOT = os.path.join(HERE, 'grading')
+
 TOKEN = os.environ['API_TOKEN']
 COURSE_ID = os.environ['COURSE_ID']
 COURSES_ROOT = 'https://canvas.instructure.com/api/v1/courses'
@@ -36,7 +39,8 @@ def get_course_students(course_id):
 
 def get_course_student_names(course_id):
     """Return json information on students in course specified by course ID."""
-    return [student['name'] for student in get_course_students(course_id)]
+    return [student['name'] for student in get_course_students(course_id)
+            if student['name'] is not 'Test Student']
 
 
 def get_module_assignment_names(items_url):
@@ -46,26 +50,38 @@ def get_module_assignment_names(items_url):
             yield item['title']
 
 
-def convert_to_dirname(name):
+def make_dir_path(path, name):
     """Return new string with no punctuation and spaces replaced with '-'.."""
     name = re.sub(BAD_CHARS_PAT, '', name)
-    return re.sub('\s+', '-', name)
+    name = re.sub('\s+', '-', name)
+    return os.path.join(path, name)
+
+
+def make_directory(path):
+    """Create a new directory with the given dirname."""
+    try:
+        os.mkdir(path)
+    except IOError:
+        pass
 
 
 def main(course_id):
     """Create local directory tree for grading assignments."""
     student_names = get_course_student_names(course_id)
     for module in get_course_modules(course_id):
-        module_name = convert_to_dirname(module['name'])
-        print('Making module dir named {}'.format(module_name))
+        module_path = make_dir_path(ROOT, module['name'])
+        print('Making module dir: {}'.format(module_path))
+        make_directory(module_path)
 
         for asgn_name in get_module_assignment_names(module['items_url']):
-            asgn_name = convert_to_dirname(asgn_name)
-            print('\tMaking assignment dir named {}'.format(asgn_name))
+            asgn_path = make_dir_path(module_path, asgn_name)
+            print('\tMaking assignment dir: {}'.format(asgn_path))
+            make_directory(asgn_path)
 
             for stu_name in student_names:
-                stu_name = convert_to_dirname(stu_name)
-                print('\t\tMaking student dir named {}'.format(stu_name))
+                stu_path = make_dir_path(asgn_path, stu_name)
+                print('\t\tMaking student dir: {}'.format(stu_path))
+                make_directory(stu_path)
 
 
 if __name__ == '__main__':

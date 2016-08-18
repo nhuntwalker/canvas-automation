fea35d85c6a147183400cf784188c6b995c00412
Will Weatherford <weatherford.william@gmail.com>
2016-08-17 20:13:55 -0700

    Importing argparse; setting default values for root name etc.

--- auto_canvas.py
+++ auto_canvas.py
@@ -11,17 +11,17 @@ from __future__ import unicode_literals
 import os
 import re
 import sys
+import argparse
 import requests
 from subprocess import call
 from string import punctuation
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-ROOT_NAME = 'grading'
+DEFAULT_ROOT_NAME = 'grading'
 
 TOKEN = os.environ['API_TOKEN']
 COURSE_ID = os.environ['COURSE_ID']
 API_ROOT = 'https://canvas.instructure.com/api/v1/'
-COURSES_ROOT = 'https://canvas.instructure.com/api/v1/courses'
 DEFAULT_PARAMS = {'access_token': TOKEN, 'per_page': 999999}
 BAD_CHARS_PAT = re.compile(r'[' + re.escape(punctuation) + r']+')
 GITHUB_REPO_PAT = re.compile(r'https://github.com/.+/.+')
@@ -45,12 +45,12 @@ def joined_api_request(*args, **kwargs):
 
 def get_course_modules(course_id):
     """Return list of modules of the course specified by ID."""
-    return joined_api_request(COURSES_ROOT, course_id, 'modules')
+    return joined_api_request(API_ROOT, 'courses', course_id, 'modules')
 
 
 def get_course_students(course_id):
     """Return list of student dicts of the course specified by ID."""
-    return joined_api_request(COURSES_ROOT, course_id, 'students')
+    return joined_api_request(API_ROOT, 'courses', course_id, 'students')
 
 
 def get_module_assignments(module):
@@ -167,7 +167,7 @@ if __name__ == '__main__':
         print('Invalid directory order acronym.')
         sys.exit()
 
-    root = os.path.join(HERE, ROOT_NAME)
+    root = os.path.join(HERE, DEFAULT_ROOT_NAME)
     make_directory(root)
 
     for module, asgn, stu, sub in all_course_combos(COURSE_ID):

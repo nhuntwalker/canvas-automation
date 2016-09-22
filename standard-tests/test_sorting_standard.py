c55c0e44e1db2c5a10474eb390de5f31aa613860
Will Weatherford <weatherford.william@gmail.com>
2016-09-22 10:51:58 -0700

    Added comment for later improvement.

--- standard-tests/test_sorting_standard.py
+++ standard-tests/test_sorting_standard.py
@@ -13,6 +13,9 @@ module = import_module(MODULENAME)
 funcdef = getattr(module, FUNCNAME)
 
 
+# Add tests for stability using anonymous but unique comparable objects
+
+
 @pytest.mark.parametrize('sequence', TEST_CASES)
 def test_sort(sequence):
     """Test that submitted sorting algo produces same result as builtin."""

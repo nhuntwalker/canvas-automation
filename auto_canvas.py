9bf4bc415ee4a10c7aa906c2c8faecf85fde8d2d
Will Weatherford <weatherford.william@gmail.com>
2017-02-27 21:44:39 -0800

    Removed commented out pdb.

--- auto_canvas.py
+++ auto_canvas.py
@@ -198,8 +198,6 @@ if __name__ == '__main__':
             stu['name'], asgn['name'], sub['url'])
         )
 
-        # import pdb;pdb.set_trace()
-
         # download .py or other files
         path = make_dir_path(root, asgn, stu, dir_order)
         make_directory(path)
